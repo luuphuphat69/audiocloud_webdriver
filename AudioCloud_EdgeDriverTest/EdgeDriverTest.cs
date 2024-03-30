3ce7ecf8495e97ca0faba016ff904d1951093804
tin <let042003@gmail.com>
2024-03-30 09:02:11 +0700

    first commit

--- AudioCloud_EdgeDriverTest/EdgeDriverTest.cs
+++ AudioCloud_EdgeDriverTest/EdgeDriverTest.cs
@@ -10,6 +10,8 @@ using OfficeOpenXml;
 using System.Security.Cryptography;
 using IdentityModel.Client;
 using AudioCloud_EdgeDriverTest;
+using System.Runtime.CompilerServices;
+using System.Reflection.Metadata;
 
 namespace AudioCloud_ChromeDriverTest
 {
@@ -219,6 +221,164 @@ namespace AudioCloud_ChromeDriverTest
 
             var aplayer = _driver.FindElement(By.Id("aplayer"));
             Assert.IsTrue(aplayer.Displayed, "Failed");
+        }
+
+
+
+        //tín comment
+
+        public static IEnumerable<object[]> ReadTestDatacommentFromExcel()
+        {
+            var testData = new List<object[]>();
+
+            using (var excelPackage = new ExcelPackage(new System.IO.FileInfo("DataTest.xlsx")))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets["comment"];
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    var query = worksheet.Cells[row, 1].Value?.ToString();
+                    testData.Add(new object[] { query });
+                }
+            }
+
+            return testData;
+        }
+
+
+        [TestMethod]
+        [DynamicData(nameof(ReadTestDatacommentFromExcel),DynamicDataSourceType.Method)]
+        public async Task comment(string comment)
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = url + "/login";
+
+            _driver.FindElement(By.Id("account")).SendKeys("tin7979");
+            _driver.FindElement(By.Id("password")).SendKeys("tin7979");
+
+            _driver.FindElement(By.Id("submit")).Submit();
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            IAlert alert = _driver.SwitchTo().Alert();
+            alert.Accept();  
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            _driver.FindElement(By.LinkText("Cầu vồng khuyết")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            var input =  _driver.FindElement(By.CssSelector("[type='text'][placeholder = 'Write a comment']"));
+            input.SendKeys(comment);
+            input.Submit();
+            try
+            {
+                Assert.IsTrue(_driver.Url.Contains("/details/AUDIO52890086"));
+                UpdateTestResultInExcel("comment", rowIndex, 2, "Passed");
+            }
+            catch (AssertFailedException ex)
+            {
+                UpdateTestResultInExcel("comment", rowIndex, 2, "Failed");
+                throw ex;
+            }
+            finally
+            {
+                rowIndex++;
+            }
+
+        }
+        //tải bài hát về thiết bị khi đã đăng nhập
+        [TestMethod]
+        public async Task dowloadsong()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = url + "/login";
+
+            _driver.FindElement(By.Id("account")).SendKeys("tin7979");
+            _driver.FindElement(By.Id("password")).SendKeys("tin7979");
+
+            _driver.FindElement(By.Id("submit")).Submit();
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            IAlert alert = _driver.SwitchTo().Alert();
+            alert.Accept();
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            _driver.FindElement(By.LinkText("Cầu vồng khuyết")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+            js.ExecuteScript("document.body.style.transform = 'scale(0.9)';");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            var button = _driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/button[3]/div[2]"));
+            button.Click();
+            await Task.Delay(TimeSpan.FromSeconds(5));       
+
+            var ifUrl = _driver.Url.Contains("storage.googleapis.com");
+            if (ifUrl)
+            {
+                Assert.IsTrue(ifUrl, "Passed");
+            }
+            else
+            {
+                Assert.IsFalse(ifUrl, "failed");
+
+            }
+        }
+        //tải bài hát về thiết bị khi chưa đăng nhập
+        [TestMethod]
+        public async Task dowloadsongnologin()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = url + "/home";
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            _driver.FindElement(By.LinkText("Cầu vồng khuyết")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+            js.ExecuteScript("document.body.style.transform = 'scale(0.9)';");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            var button = _driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/button[3]/div[2]"));
+            button.Click(); 
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            var ifUrl = _driver.Url.Contains("storage.googleapis.com");
+            if (ifUrl)
+            {
+                Assert.IsTrue(ifUrl, "Passed");
+            }
+            else
+            {
+                Assert.IsFalse(ifUrl, "failed");
+
+            }
+        }
+        ////xóa bài hát 
+        [TestMethod]
+        public async Task deletesong()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = url + "/login";
+
+            _driver.FindElement(By.Id("account")).SendKeys("tin7979");
+            _driver.FindElement(By.Id("password")).SendKeys("tin7979");
+
+            _driver.FindElement(By.Id("submit")).Submit();
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            IAlert alert = _driver.SwitchTo().Alert();
+            alert.Accept();
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            _driver.FindElement(By.CssSelector("#navbarDropdownMenuLink")).Click();
+            _driver.FindElement(By.LinkText("Tài khoản")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            _driver.FindElement(By.LinkText("Danh sách phát")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+            js.ExecuteScript("window.scrollTo(0,400);");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            _driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div")).Click();
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            var ktdk = _driver.FindElement(By.CssSelector("#root > div > div.card.card-body.mx-3.mx-md-4.mt-n6 > div:nth-child(2) > div > div.overlay > div > div > div > div:nth-child(1) > button"));
+            ktdk.Click();
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            Assert.IsTrue(ktdk.Displayed, "Failed");
         }
     }
 }
